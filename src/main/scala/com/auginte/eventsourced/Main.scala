19e1f7a9fe8af67046d34ecc01fc5f30094e76ba
Aurelijus Banelis <aurelijus@banelis.lt>
2016-09-07 23:39:44 +0300

    Storing data, using akka streams Graph DSL
    Using one file per project and pushing all events (currently) without validation or changes.

--- src/main/scala/com/auginte/eventsourced/Main.scala
+++ src/main/scala/com/auginte/eventsourced/Main.scala
@@ -1,12 +1,13 @@
 package com.auginte.eventsourced
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.javadsl.{model => jm}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers._
-import akka.stream.ActorMaterializer
+import akka.stream._
 import akka.stream.scaladsl.{Source, _}
 import akka.util.{ByteString, Timeout}
 
@@ -14,6 +15,10 @@ import scala.concurrent.duration._
 import scala.util.Random
 
 object Main {
+  // Storage example
+  val storagePath = env("auginte.storage.path", "./data")
+  val storage = new Storage(storagePath)
+
   def env(param: String, default: String = ""): String = sys.props.getOrElse(param, default)
 
   def main(args: Array[String]) {
@@ -28,13 +33,6 @@ object Main {
     implicit val timeout = Timeout(5.seconds)
     implicit val executionContext = system.dispatcher
 
-    // Storage example
-    val storagePath = env("auginte.storage.path", "./data")
-    val storage = new Storage(storagePath)
-    val exampleCluster = "test"
-    val success = storage.append(exampleCluster, System.nanoTime() + "DATA")
-    println(success)
-//    storage.readAll(exampleCluster).runForeach(println)
 
     def date = new java.util.Date().toString
 
@@ -74,7 +72,7 @@ object Main {
       `Access-Control-Allow-Origin`.*
 
     val flow: Flow[HttpRequest, HttpResponse, Any] = Flow[HttpRequest].map {
-      case r @ HttpRequest(GET, Uri.Path("/stream"), _, _, _) =>
+      case r@HttpRequest(GET, Uri.Path("/stream"), _, _, _) =>
         println("Debug: Stream")
 
         println(r)
@@ -91,7 +89,7 @@ object Main {
 
         )
 
-      case r @ HttpRequest(GET, uri, _, _, _) if uri.path.startsWith(Uri.Path("/project/")) =>
+      case r@HttpRequest(GET, uri, _, _, _) if uri.path.startsWith(Uri.Path("/project/")) =>
         val project = uri.path.reverse.head.toString
 
         HttpResponse(
@@ -100,15 +98,14 @@ object Main {
           HttpEntity(ContentType(MediaTypes.`text/html`, HttpCharsets.`UTF-8`), Html.page(project))
         )
 
-      case r @ HttpRequest(POST, uri, _, entity, _) if uri.path.startsWith(Uri.Path("/project/")) =>
+      case r@HttpRequest(POST, uri, _, entity, _) if uri.path.startsWith(Uri.Path("/project/")) =>
         val project = uri.path.reverse.head.toString
 
-        val commands = entity.dataBytes
-           .via(Framing.delimiter(ByteString("\n"), Int.MaxValue))
-           .map(_.utf8String)
-           .map(a => {println(a); a})
-
-        val array = commands.runReduce((a, b) => a + "," + b).map(i => "[" + i + "]").map(ByteString.apply)
+        val array = entity.dataBytes
+          .via(Framing.delimiter(ByteString("\n"), Int.MaxValue))
+          .map(_.utf8String)
+          .via(processCommands(project))
+          .runReduce((a, b) => a + "," + b).map(i => "[" + i + "]").map(ByteString.apply)
 
         HttpResponse(
           StatusCodes.OK,
@@ -116,7 +113,7 @@ object Main {
           HttpEntity(ContentType(MediaTypes.`application/json`), Source.fromFuture(array))
         )
 
-      case r @ HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+      case r@HttpRequest(GET, Uri.Path("/"), _, _, _) =>
         println(r)
 
         HttpResponse(
@@ -130,6 +127,25 @@ object Main {
     }
 
     val bindingFuture = Http().bindAndHandle(flow, host, port)
+  }
+
+  def processCommands(project: String) = GraphDSL.create() { implicit b =>
+    import GraphDSL.Implicits._
+
+    val input = b.add(Broadcast[String](3))
+    val output = b.add(Flow[(String)].buffer(5, OverflowStrategy.backpressure))
+
+    input ~> logEvent ~> Sink.ignore
+    input ~> storeToDatabase(project) ~> Sink.ignore
+    input ~> output
+
+    FlowShape(input.in, output.outlet)
+  }
+
+  def logEvent: Flow[String, Unit, NotUsed] = Flow.fromFunction(data => println(s"Log: $data"))
+
+  def storeToDatabase(project: String): Flow[String, Boolean, NotUsed] = Flow.fromFunction{ data =>
+    storage.append(project, data)
   }
 }
 
