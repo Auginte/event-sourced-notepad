1c556de36ff3f1a699b16b740a8e5d9e6c88f2e6
Aurelijus Banelis <aurelijus@banelis.lt>
2016-10-16 21:54:52 +0300

    v0.0.6 Moving elements across desktop and mobile

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import xerial.sbt.Pack.{pack => sbtPack, _}
 
 object Build extends sbt.Build {
   val buildName = "auginte-event-sourced"
-  val buildVersion = "0.0.6-SNAPSHOT"
+  val buildVersion = "0.0.6"
   val buildScalaVersion = "2.11.8"
   val buildOptions = Seq("-feature", "-unchecked", "-deprecation", "-encoding", "utf8")
   val akkaVersion = "2.4.9"
